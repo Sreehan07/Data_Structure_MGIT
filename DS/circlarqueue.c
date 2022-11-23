ae1593c7e113e24a810ff0a770cf58c1bb0c8401
Sreehan07 <82800385+Sreehan07@users.noreply.github.com>
2022-11-23 23:05:46 +0530

    Create circlarqueue.c

---
+++ DS/circlarqueue.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int Queue[5];
+
+int insert = 0;
+int delete1 = -1;
+
+void enque(int data)
+{
+    if (insert+1 == delete1)
+    {
+        printf("Queue is full:\n");
+    }
+    else
+    {
+        // Bringing back the insertion pointer to starting after the pointer reaches the end
+        if (insert == 5)
+        {
+            insert = 0;
+        }
+        Queue[insert] = data;
+        insert++;
+    }
+}
+
+void deque()
+{
+
+    delete1++;
+    if (delete1 == insert)
+    {
+        printf("Queue is empty:\n");
+    }
+    else
+    {
+        // Brining back the deletion pointer to starting after the pointer reaches the end
+        if (delete1 == 5)
+        {
+            delete1 = 0;
+        }
+        Queue[delete1] = 0;
+    }
+}
+
+int main()
+{
+    for (int i = 1; i < 4; i++)
+    {
+        enque(i * 10);
+    }
+    for (int i = 1; i < 5; i++)
+    {
+        deque();
+    }
+    for (int i = 1; i < 7; i++)
+    {
+        enque(i * 10);
+    }
+}
